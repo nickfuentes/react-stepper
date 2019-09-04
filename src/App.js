165f2788a1cd608298be3fad7ad8f54e1c71d81a
Full Name <nickfuentes24@gmail.com>
2019-09-03 20:26:40 -0500

    Created a react stepper using a class component.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      counter: 0
+    }
+  }
+
+  incrementCounter = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
+
+  decrementCounter = () => {
+    this.setState({
+      counter: this.state.counter - 1
+    })
+  }
+
+  render() {
+    return (
+      <div class="counter">
+        <button onClick={this.incrementCounter}>+</button>
+        <h3>{this.state.counter}</h3>
+        <button onClick={this.decrementCounter}>-</button>
+      </div>
+    )
+  }
+
 }
 
 export default App;
